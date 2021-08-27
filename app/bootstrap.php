98b4023f6095efcb4e8a6b9ee6e1c83b6de7fe97
Yuri <forgameshit.18@gmail.com>
2021-08-27 12:27:14 +0600

    Made changes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,5 +3,8 @@
 spl_autoload_register(function(string $className) {
     $split = explode('\\', $className);
     $cnt = count($split);
-    require_once pathJoin(APP_PATH, $split[$cnt - 1] . '.php');
+    $name = $split[$cnt - 1];
+    $nsList = ['Legax\\Core', 'Legax\\UI\\Views\\Support', 'Legax\\UI\\Views'];
+    $ns = array_slice($split, 0, $cnt - 1);
+    if(in_array($ns, $nsList)) require_once pathJoin(APP_PATH, $name . '.php');
 });
